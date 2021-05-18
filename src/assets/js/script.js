4a48fc62a429c842ed9fa1770fc3ae527415a494
Max Krivosheia <maxkr1203@gmail.com>
2021-05-18 07:55:59 +0300

    fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,10 +1,11 @@
 document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let height = document.getElementById("height").value / 100;
-    let weight = document.getElementById("weight").value;
+    let height = Number(document.getElementById("height").value / 100);
+    let weight = Number(document.getElementById("weight").value);
     let bmi = weight / height ** 2;
 
+    console.log(bmi);
     document.getElementById("result").innerHTML = bmi.toFixed(2);
 
     document.getElementById("below").classList.remove("active");
@@ -14,17 +15,17 @@ document.getElementById("form").addEventListener("submit", function (event) {
     document.getElementById("Obesity2").classList.remove("active");
     document.getElementById("Obesity3").classList.remove("active");
 
-    if (bmi < 18.49) {
+    if (bmi <= 18.499999999999999999999999) {
         document.getElementById("below").classList.add("active");
-    } else if ((bmi > 18.5) & (bmi < 24.99)) {
+    } else if (bmi >= 18.5 && bmi <= 24.9999999999999999999999) {
         document.getElementById("normal").classList.add("active");
-    } else if ((bmi > 25) & (bmi < 29.99)) {
+    } else if (bmi >= 25 && bmi <= 29.9999999999999999999999) {
         document.getElementById("Overweight").classList.add("active");
-    } else if ((bmi > 30) & (bmi < 34.99)) {
+    } else if (bmi >= 30 && bmi <= 34.9999999999999999999999) {
         document.getElementById("Obesity1").classList.add("active");
-    } else if ((bmi > 35) & (bmi < 39.99)) {
+    } else if (bmi >= 35 && bmi <= 39.999999999999999999999999) {
         document.getElementById("Obesity2").classList.add("active");
-    } else if (bmi > 40) {
+    } else if (bmi >= 40) {
         document.getElementById("Obesity3").classList.add("active");
     }
 });
